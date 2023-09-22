7d40d7667fa64a2665c656c5e7b89e22bd3ada94
danielvdg <daniel.vdg@hotmail.com>
2023-09-22 17:15:03 -0300

    first commit

---
+++ src/main/java/br/com/fila/app/filadeespera/FiladeesperaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fila.app.filadeespera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiladeesperaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiladeesperaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fila/app/filadeespera/FiladeesperaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fila.app.filadeespera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiladeesperaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
