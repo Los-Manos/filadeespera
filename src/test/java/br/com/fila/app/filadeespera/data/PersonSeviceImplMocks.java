c91ab39d8888f2a9d871236cd56a759f38a30fc8
danielvdg <daniel.vdg@hotmail.com>
2023-10-24 23:48:10 -0300

    teste commit test

--- src/test/java/br/com/fila/app/filadeespera/data/PersonSeviceImplMocks.java
+++ src/test/java/br/com/fila/app/filadeespera/data/PersonSeviceImplMocks.java
@@ -1,54 +1,54 @@
-// package br.com.fila.app.filadeespera.data;
+package br.com.fila.app.filadeespera.data;
 
-// import java.time.LocalDate;
-// import java.util.Optional;
+import java.time.LocalDate;
+import java.util.Optional;
 
-// import br.com.fila.app.filadeespera.model.entity.Person;
-// import lombok.AccessLevel;
-// import lombok.NoArgsConstructor;
+import br.com.fila.app.filadeespera.model.entity.Person;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
-// @NoArgsConstructor(access = AccessLevel.PRIVATE)
-// public class PersonSeviceImplMocks {
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class PersonSeviceImplMocks {
 
-//     public static Person fullPersonDataMock() {
-//         var person = Person.builder()
-//                 .id(1L)
-//                 .name("Daniel")
-//                 .bith(LocalDate.of(1994, 5, 11))
-//                 .cpf("13599177724")
-//                 .email("daniel.vdg@hotmail.com")
-//                 .sus("1484946848618")
-//                 .build();
+    public static Person fullPersonDataMock() {
+        var person = Person.builder()
+                .id(1L)
+                .name("Daniel")
+                .bith(LocalDate.of(1994, 5, 11))
+                .cpf("13599177724")
+                .email("daniel.vdg@hotmail.com")
+                .sus("1484946848618")
+                .build();
 
-//         return person;
-//     }
+        return person;
+    }
 
 
-//      public static Optional<Person> fullOptinalPersonDataMock() {
-//         Optional<Person> person = Optional.of(Person.builder()
-//                 .id(1L)
-//                 .name("Daniel")
-//                 .bith(LocalDate.of(1994, 5, 11))
-//                 .cpf("13599177724")
-//                 .email("daniel.vdg@hotmail.com")
-//                 .sus("1484946848618")
-//                 .build());
+     public static Optional<Person> fullOptinalPersonDataMock() {
+        Optional<Person> person = Optional.of(Person.builder()
+                .id(1L)
+                .name("Daniel")
+                .bith(LocalDate.of(1994, 5, 11))
+                .cpf("13599177724")
+                .email("daniel.vdg@hotmail.com")
+                .sus("1484946848618")
+                .build());
 
-//         return person;
-//     }
+        return person;
+    }
 
     
-//     public static Person personNullDataMock() {
-//         var person = Person.builder()
-//                 .id(null)
-//                 .name(null)
-//                 .bith(LocalDate.now())
-//                 .cpf(null)
-//                 .email(null)
-//                 .sus(null)
-//                 .build();
+    public static Person personNullDataMock() {
+        var person = Person.builder()
+                .id(null)
+                .name(null)
+                .bith(LocalDate.now())
+                .cpf(null)
+                .email(null)
+                .sus(null)
+                .build();
 
-//         return person;
-//     }
+        return person;
+    }
 
-// }
+}
--- src/test/java/br/com/fila/app/filadeespera/service/impl/PersonServiceImplTest.java
+++ src/test/java/br/com/fila/app/filadeespera/service/impl/PersonServiceImplTest.java
@@ -1,92 +1,92 @@
-// package br.com.fila.app.filadeespera.service.impl;
+package br.com.fila.app.filadeespera.service.impl;
 
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-// import static org.junit.jupiter.api.Assertions.assertNotNull;
-// import static org.junit.jupiter.api.Assertions.assertThrows;
-// import static org.mockito.ArgumentMatchers.anyLong;
-// import static org.mockito.Mockito.times;
-// import static org.mockito.Mockito.verify;
-// import static org.mockito.Mockito.verifyNoMoreInteractions;
-// import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
-// import java.util.Optional;
+import java.util.Optional;
 
-// import org.junit.jupiter.api.Test;
-// import org.mockito.InjectMocks;
-// import org.mockito.Mock;
-// import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
 
-// import br.com.fila.app.filadeespera.data.PersonSeviceImplMocks;
-// import br.com.fila.app.filadeespera.exception.BusinessException;
-// import br.com.fila.app.filadeespera.model.entity.Person;
-// import br.com.fila.app.filadeespera.repository.PersonRepository;
-// import br.com.fila.app.filadeespera.service.PersonService;
+import br.com.fila.app.filadeespera.data.PersonSeviceImplMocks;
+import br.com.fila.app.filadeespera.exception.BusinessException;
+import br.com.fila.app.filadeespera.model.entity.Person;
+import br.com.fila.app.filadeespera.repository.PersonRepository;
+import br.com.fila.app.filadeespera.service.PersonService;
 
-// @SpringBootTest
-// class PersonServiceImplTest {
+@SpringBootTest
+class PersonServiceImplTest {
 
     
-//     @InjectMocks
-//     private PersonServiceImpl personServiceImpl;
+    @InjectMocks
+    private PersonServiceImpl personServiceImpl;
 
-//     @Mock
-//     private PersonRepository personRepository;
+    @Mock
+    private PersonRepository personRepository;
 
-//     @Mock
-//     private PersonService personService;
+    @Mock
+    private PersonService personService;
 
-//     Person person;
+    Person person;
     
-//     @Test
-//     void shouldCreatePersonWithSuccess() {
-//         var person = PersonSeviceImplMocks.fullPersonDataMock();
-//         personRepository.save(person);
-//         verify(personRepository, times(1)).save(person);
-//         verifyNoMoreInteractions(personRepository);
-//         when(personServiceImpl.save(person)).thenReturn(person);
-//         Person personCurrent = personRepository.save(person);
-//         assertEquals(person, personCurrent);
-//     }
+    @Test
+    void shouldCreatePersonWithSuccess() {
+        var person = PersonSeviceImplMocks.fullPersonDataMock();
+        personRepository.save(person);
+        verify(personRepository, times(1)).save(person);
+        verifyNoMoreInteractions(personRepository);
+        when(personServiceImpl.save(person)).thenReturn(person);
+        Person personCurrent = personRepository.save(person);
+        assertEquals(person, personCurrent);
+    }
 
-//     @Test
-//     void shouldThrowBusinessExceptionWhenNotSave() {        
-//         when(personRepository.save(person)).
-//                 thenThrow(new BusinessException("Erro ao salvar person"));
+    @Test
+    void shouldThrowBusinessExceptionWhenNotSave() {        
+        when(personRepository.save(person)).
+                thenThrow(new BusinessException("Erro ao salvar person"));
         
-//         BusinessException businessException  = assertThrows(
-//                 BusinessException.class, 
-//                 () -> personServiceImpl.save(person)
-//         );
+        BusinessException businessException  = assertThrows(
+                BusinessException.class, 
+                () -> personServiceImpl.save(person)
+        );
         
-//         verify(personRepository,times(0)).save(person);
-//         assertEquals("Erro ao salvar person", businessException.getMessage());
-//     }
+        verify(personRepository,times(0)).save(person);
+        assertEquals("Erro ao salvar person", businessException.getMessage());
+    }
 
-//     @Test
-//     void shouldFindByIdPersonWithSucess(){
-//         var personOptinal = PersonSeviceImplMocks.fullOptinalPersonDataMock();
-//         when(personRepository.findById(anyLong())).thenReturn(personOptinal);
-//         verify(personRepository,times(0)).findById(anyLong());
-//         verifyNoMoreInteractions(personRepository);
-//         Person response = personServiceImpl.findById(anyLong());
-//         assertNotNull(response);
-//         assertEquals(Person.class,response.getClass());
-//     }
+    @Test
+    void shouldFindByIdPersonWithSucess(){
+        var personOptinal = PersonSeviceImplMocks.fullOptinalPersonDataMock();
+        when(personRepository.findById(anyLong())).thenReturn(personOptinal);
+        verify(personRepository,times(0)).findById(anyLong());
+        verifyNoMoreInteractions(personRepository);
+        Person response = personServiceImpl.findById(anyLong());
+        assertNotNull(response);
+        assertEquals(Person.class,response.getClass());
+    }
     
-//     @Test
-//     void shouldThrowBusinessExcptionWhenPersonIdIsEmpty(){
+    @Test
+    void shouldThrowBusinessExcptionWhenPersonIdIsEmpty(){
        
-//         when(personRepository.findById(anyLong()))
-//                 .thenThrow(BusinessException.class);
+        when(personRepository.findById(anyLong()))
+                .thenThrow(BusinessException.class);
        
-//         BusinessException businessExceptionFindById = assertThrows
-//                 (BusinessException.class,
-//                 ()-> personServiceImpl.findById(null)
-//         );
+        BusinessException businessExceptionFindById = assertThrows
+                (BusinessException.class,
+                ()-> personServiceImpl.findById(null)
+        );
 
-//        assertEquals("id do person não encontrado", businessExceptionFindById.getMessage());
+       assertEquals("id do person não encontrado", businessExceptionFindById.getMessage());
         
-//     }
+    }
 
     
-// }
+}
